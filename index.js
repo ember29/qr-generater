e759c391459f16e58bce9ad1874026194bb74981
vinayak durgule <vinayaksunildurgule@gmail.com>
2025-07-21 20:56:46 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+
 import inquirer from "inquirer";
 import qr from 'qr-image';
 import fs from 'fs';
 inquirer
   .prompt([
-    /* Pass your questions in here */
     {message:"Type in your URL",
         name:"URL"
     }
@@ -25,14 +20,14 @@ inquirer
       fs.appendFileSync('url.txt', 'url');
       console.log('The "data to append" was appended to file!');
     } catch (err) {
-      /* Handle the error */
+  
     } 
-    // Use user feedback for... whatever!!
+    
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+    
     } else {
-      // Something else went wrong
+    
     }
   });
